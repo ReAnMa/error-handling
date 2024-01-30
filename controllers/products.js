4f0f0ed99b355c65ead6e5f26a3d808f12cc15f2
korzeniewskiraf <korzeniewskiraf@hermesworld.com>
2024-01-30 19:40:34 +0100

    -in products.js übergabe req.body.description replaced by req.body.title

--- controllers/products.js
+++ controllers/products.js
@@ -5,7 +5,7 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.getPostProduct = (req, res, next) => {
-    const product = new Product(req.body.description)
+    const product = new Product(req.body.title)
     product.save();
     res.redirect('/');
 };
